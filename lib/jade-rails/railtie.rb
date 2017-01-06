0e41a23018aa0d8443364f9e8fc762f853db9e2f
Yaroslav Konoplov <eahome00@gmail.com>
2017-01-06 14:59:33 +0200

    Refactor railties

--- lib/jade-rails/railtie.rb
+++ lib/jade-rails/railtie.rb
@@ -9,29 +9,47 @@ module Jade
     config.jade.pretty        = Rails.env.development?
     config.jade.compile_debug = Rails.env.development?
 
-    def configure_assets(app)
-      if config.respond_to?(:assets) && config.assets.respond_to?(:configure)
-        # Rails 4.x
-        config.assets.configure { |env| yield(env) }
-      else
-        # Rails 3.2
-        yield(app.assets)
-      end
-    end
-
-    initializer 'sprockets.jade', group: :all, after: 'sprockets.environment' do |app|
-      configure_assets(app) do |env|
+    initializer 'sprockets.jade.transformer', after: 'sprockets.environment', group: :all do |app|
+      access_assets_environment app do |env|
         # Sprockets 2, 3, and 4
         if env.respond_to?(:register_transformer)
           env.register_mime_type 'text/x-jade', extensions: ['.jade']
-          env.register_transformer 'text/x-jade', 'application/javascript', Jade::SprocketsTransformer
+          env.register_transformer 'text/x-jade', 'application/javascript', Jade::Sprockets::Transformer
         end
 
         if env.respond_to?(:register_engine)
-          args = ['.jade', Jade::SprocketsTransformer]
-          args << { mime_type: 'text/x-jade', silence_deprecation: true } if Sprockets::VERSION.start_with?('3')
+          args = ['.jade', Jade::Sprockets::Transformer]
+          args << { mime_type: 'text/x-jade', silence_deprecation: true } if ::Sprockets::VERSION.start_with?('3')
           env.register_engine(*args)
         end
+      end
+    end
+
+    initializer 'sprockets.jade.runtime', after: 'sprockets.environment', group: :all do |app|
+      access_assets_config app do |assets|
+        assets.precompile += %w( jade/runtime.js )
+        assets.paths      += [File.expand_path('../../../vendor/assets/javascripts', __FILE__)]
+      end
+    end
+
+  private
+    def access_assets_config(app)
+      if config.respond_to?(:assets) && config.assets.respond_to?(:configure)
+        # Rails 4.x 5.x
+        yield config.assets
+      else
+        # Rails 3.2
+        yield app.assets
+      end
+    end
+
+    def access_assets_environment(app)
+      if config.respond_to?(:assets) && config.assets.respond_to?(:configure)
+        # Rails 4.x 5.x
+        config.assets.configure { |env| yield env }
+      else
+        # Rails 3.2
+        yield app.assets
       end
     end
   end
--- lib/pug-rails/railtie.rb
+++ lib/pug-rails/railtie.rb
@@ -9,29 +9,47 @@ module Pug
     config.pug.pretty        = Rails.env.development?
     config.pug.compile_debug = Rails.env.development?
 
-    def configure_assets(app)
-      if config.respond_to?(:assets) && config.assets.respond_to?(:configure)
-        # Rails 4.x
-        config.assets.configure { |env| yield(env) }
-      else
-        # Rails 3.2
-        yield(app.assets)
-      end
-    end
-
-    initializer 'sprockets.pug', group: :all, after: 'sprockets.environment' do |app|
-      configure_assets(app) do |env|
+    initializer 'sprockets.pug.transformer', after: 'sprockets.environment', group: :all do |app|
+      access_assets_environment app do |env|
         # Sprockets 2, 3, and 4
         if env.respond_to?(:register_transformer)
           env.register_mime_type 'text/x-pug', extensions: ['.pug']
-          env.register_transformer 'text/x-pug', 'application/javascript+function', Pug::SprocketsTransformer
+          env.register_transformer 'text/x-pug', 'application/javascript+function', Pug::Sprockets::Transformer
         end
 
         if env.respond_to?(:register_engine)
-          args = ['.pug', Pug::SprocketsTransformer]
-          args << { mime_type: 'text/x-pug', silence_deprecation: true } if Sprockets::VERSION.start_with?('3')
+          args = ['.pug', Pug::Sprockets::Transformer]
+          args << { mime_type: 'text/x-pug', silence_deprecation: true } if ::Sprockets::VERSION.start_with?('3')
           env.register_engine(*args)
         end
+      end
+    end
+
+    initializer 'sprockets.pug.runtime', after: 'sprockets.environment', group: :all do |app|
+      access_assets_config app do |assets|
+        assets.precompile += %w( pug/runtime.js )
+        assets.paths      += [File.expand_path('../../../vendor/assets/javascripts', __FILE__)]
+      end
+    end
+
+  private
+    def access_assets_config(app)
+      if config.respond_to?(:assets) && config.assets.respond_to?(:configure)
+        # Rails 4.x 5.x
+        yield config.assets
+      else
+        # Rails 3.2
+        yield app.assets
+      end
+    end
+
+    def access_assets_environment(app)
+      if config.respond_to?(:assets) && config.assets.respond_to?(:configure)
+        # Rails 4.x 5.x
+        config.assets.configure { |env| yield env }
+      else
+        # Rails 3.2
+        yield app.assets
       end
     end
   end
